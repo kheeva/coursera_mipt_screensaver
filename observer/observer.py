b7990a8d851167095126c68f9f3d268bbedd48bf
kheeva <kheeva@ya.ru>
2019-02-19 12:40:28 +0300

    add observer pattern

---
+++ observer/observer.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+
+class Engine:
+    pass
+
+
+class ObservableEngine(Engine):
+    def __init__(self):
+        self.__subscribers = set()
+
+    def subscribe(self, subscriber):
+        self.__subscribers.add(subscriber)
+
+    def unsubscribe(self, subscriber):
+        if subscriber in self.__subscribers:
+            self.__subscribers.remove(subscriber)
+
+    def notify(self, message):
+        for subscriber in self.__subscribers:
+            subscriber.update(message)
+
+
+class AbstractObserver(ABC):
+    @abstractmethod
+    def update(self, achievement):
+        pass
+
+
+class ShortNotificationPrinter(AbstractObserver):
+    def __init__(self):
+        self.achievements = set()
+
+    def update(self, achievement):
+        self.achievements.add(achievement['title'])
+
+
+class FullNotificationPrinter(AbstractObserver):
+    def __init__(self):
+        self.achievements = []
+
+    def update(self, achievement):
+        if achievement not in self.achievements:
+            self.achievements.append(achievement)
